98ff969d2da5a5bb8b7a9c46b2a1b80bee5fdd7c
Alex McKenzie <mcalex@hey.com>
2021-06-16 23:50:40 -0400

    add filter condition

--- src/filter-condition.ts
+++ src/filter-condition.ts
@@ -1,5 +1,24 @@
-export class FilterConditionStatement {}
-
-export class FilterCondition {
-	static equals(value: unknown) {}
+export enum FilterComparison {
+	Greater = '>',
+	GreaterOrEqual = '>=',
+	Less = '<',
+	LessOrEqual = '<=',
+	BeginsWith = 'begins_with',
 }
+
+export type CompareFilterCondition<T, K extends keyof T> = [
+	K,
+	FilterComparison | `${FilterComparison}`,
+	string | number,
+];
+
+export type BetweenFilterCondition<T, K extends keyof T> = [
+	K,
+	'between',
+	string | number,
+	string | number,
+];
+
+export type FilterCondition<T, K extends keyof T> =
+	| CompareFilterCondition<T, K>
+	| BetweenFilterCondition<T, K>;
