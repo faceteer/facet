d1df50620a0ceac26982ab8328f0183b1f393909
Alex McKenzie <alex@unbreak-it.com>
2021-12-27 22:55:04 -0500

    update exports
    Update the exports so we can expose more useful types
    and utilities to users

--- index.ts
+++ index.ts
@@ -1,2 +1,28 @@
+export * as Cursor from './lib/cursor';
+export type {
+	DeleteFailure,
+	DeleteOptions,
+	DeleteResponse,
+} from './lib/delete';
 export { Facet } from './lib/facet';
-export type { FacetOptions } from './lib/facet';
+export type {
+	AddIndexOptions,
+	FacetIndex,
+	FacetOptions,
+	FacetWithIndex,
+	Validator,
+} from './lib/facet';
+export { Index } from './lib/keys';
+export type { KeyConfiguration, ShardConfiguration } from './lib/keys';
+export type {
+	PutFailure,
+	PutOptions,
+	PutResponse,
+	PutSingleItemResponse,
+} from './lib/put';
+export type {
+	PartitionQuery,
+	PartitionQueryOptions,
+	QueryOptions,
+	QueryResult,
+} from './lib/query';
